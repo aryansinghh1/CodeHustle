d4c7181cbe0a967aeeb62b8f66311d773ed0e367
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 11:32:10 +0530

    giving path for error middle ware

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import errorMiddleware from "./middleware/errorMiddleware";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
