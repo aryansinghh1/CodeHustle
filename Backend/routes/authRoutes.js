956082bcb27d7febaec8c41d6bee102516a47936
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 11:34:16 +0530

    creating router for auth

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  signup,
+  login,
+} from "../controllers/authController.js";
+
+import {
+  validateSignup,
+  validateLogin,
+} from "../validators/authValidator.js";
+
+const router = express.Router();
+
+router.post("/signup", validateSignup, signup);
+
+router.post("/login", validateLogin, login);
+
+export default router;
