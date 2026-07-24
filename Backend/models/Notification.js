55527eb61abab6e75dc9bbe7062b9f5aa921ed3b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 11:20:22 +0530

    Notification Schema

---
+++ Backend/models/Notification.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "Registration",
+        "Submission",
+        "Review",
+        "Announcement",
+        "Result",
+        "General",
+      ],
+      default: "General",
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
