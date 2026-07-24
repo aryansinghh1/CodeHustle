a0b0d5b78ff05f315f8503a022138ee17caf3f04
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 11:27:31 +0530

    Creating Authmiddleware

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = await User.findById(decoded.id).select("-password");
+
+      if (!user) {
+        return res.status(401).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+
+      if (user.isBlocked) {
+        return res.status(403).json({
+          success: false,
+          message: "Your account has been blocked.",
+        });
+      }
+
+      req.user = user;
+
+      next();
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized. Token missing.",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token.",
+    });
+  }
+};
+
+export default authMiddleware;
