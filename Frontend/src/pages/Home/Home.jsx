ff9eb132c44c0bcc1d11850525ca64d4c40ac9d2
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 14:58:39 +0530

    updating home.jsx

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import MainLayout from "../../layouts/MainLayout";
 
+import Hero from "../../components/home/Hero";
+import Stats from "../../components/home/Stats";
+
 function Home() {
   return (
     <MainLayout>
-      <h1>Home</h1>
+
+      <Hero />
+
+      <Stats />
+
     </MainLayout>
   );
 }
