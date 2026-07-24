c2b44d9f8de9b6495e35d649dd986f339f5fa036
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 16:54:04 +0530

    creating userservice

---
+++ Frontend/src/services/userService.js
@@ -0,0 +1,19 @@
+import api from "./axios";
+
+export const getAllUsers = () =>
+  api.get("/users");
+
+export const getUserById = (id) =>
+  api.get(`/users/${id}`);
+
+export const blockUser = (id) =>
+  api.put(`/users/${id}/block`);
+
+export const unblockUser = (id) =>
+  api.put(`/users/${id}/unblock`);
+
+export const deleteUser = (id) =>
+  api.delete(`/users/${id}`);
+
+export const createUser = (data) =>
+  api.post("/users/create-user", data);
