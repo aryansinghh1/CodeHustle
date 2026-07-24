e55172c58e763fb74c7af1c176242ee355243bd1
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 15:13:46 +0530

    creating hackathonjsx

---
+++ Frontend/src/services/hackathonService.js
@@ -0,0 +1,16 @@
+import api from "./axios";
+
+export const getHackathons = (params) =>
+  api.get("/hackathons", { params });
+
+export const getHackathonById = (id) =>
+  api.get(`/hackathons/${id}`);
+
+export const createHackathon = (data) =>
+  api.post("/hackathons", data);
+
+export const updateHackathon = (id, data) =>
+  api.put(`/hackathons/${id}`, data);
+
+export const deleteHackathon = (id) =>
+  api.delete(`/hackathons/${id}`);
