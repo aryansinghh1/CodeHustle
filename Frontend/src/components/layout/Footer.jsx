421b556d7560f3029b93ede245c6acf13a67d525
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 14:41:27 +0530

    creating footer

---
+++ Frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,35 @@
+function Footer() {
+  return (
+    <footer className="border-t border-gray-200 mt-20">
+
+      <div className="max-w-7xl mx-auto py-12 px-6">
+
+        <div className="flex flex-col md:flex-row justify-between items-center gap-6">
+
+          <div>
+
+            <h2 className="text-xl font-bold">
+              CodeHustle
+            </h2>
+
+            <p className="text-gray-500 mt-2">
+              Build. Compete. Innovate.
+            </p>
+
+          </div>
+
+          <div className="text-gray-500 text-sm">
+
+            © 2026 CodeHustle. All Rights Reserved.
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
