52e3901b323cd18b961843c31a2ddaaf4018b709
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-24 15:02:34 +0530

    updating home page

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -2,15 +2,22 @@ import MainLayout from "../../layouts/MainLayout";
 
 import Hero from "../../components/home/Hero";
 import Stats from "../../components/home/Stats";
+import TrustedBy from "../../components/home/TrustedBy";
+import WhyChoose from "../../components/home/WhyChoose";
+import CTA from "../../components/home/CTA";
 
 function Home() {
   return (
     <MainLayout>
-
       <Hero />
+
+      <TrustedBy />
 
       <Stats />
 
+      <WhyChoose />
+
+      <CTA />
     </MainLayout>
   );
 }
