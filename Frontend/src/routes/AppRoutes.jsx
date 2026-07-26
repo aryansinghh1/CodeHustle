b39e46acb6b4fefb5471db41a3a3fd5250e8e421
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-26 13:42:21 +0530

    updating routes

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -4,33 +4,47 @@ import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/Signup";
 import NotFound from "../pages/Error/NotFound";
+
 import Listing from "../pages/Hackathon/Listing";
 import Details from "../pages/Hackathon/Details";
+
 import TeamPage from "../pages/Team/TeamPage";
+
 import Register from "../pages/Registration/Register";
 import MyRegistrations from "../pages/Registration/MyRegistrations";
+
 import ParticipantDashboard from "../pages/Participant/Dashboard";
+
 import OrganizerDashboard from "../pages/Organizer/Dashboard";
 import CreateHackathon from "../pages/Organizer/CreateHackathon";
 import MyHackathons from "../pages/Organizer/MyHackathons";
 import EditHackathon from "../pages/Organizer/EditHackathon";
 import Registrations from "../pages/Organizer/Registrations";
+
 import MySubmissions from "../pages/Submission/MySubmissions";
 import CreateSubmission from "../pages/Submission/CreateSubmission";
 import EditSubmission from "../pages/Submission/EditSubmission";
+
 import JudgeDashboard from "../pages/Judge/Dashboard";
 import JudgeSubmissions from "../pages/Judge/Submissions";
 import ReviewSubmission from "../pages/Judge/ReviewSubmission";
+
 import Leaderboard from "../pages/Leaderboard/Leaderboard";
+
 import AdminDashboard from "../pages/Admin/Dashboard";
 import Users from "../pages/Admin/Users";
 import CreateUser from "../pages/Admin/CreateUser";
+
 import Profile from "../pages/Profile/Profile";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
   return (
     <Routes>
+
+      {/* ================= PUBLIC ROUTES ================= */}
+
       <Route path="/" element={<Home />} />
 
       <Route path="/login" element={<Login />} />
@@ -41,11 +55,9 @@ function AppRoutes() {
 
       <Route path="/hackathons/:id" element={<Details />} />
 
-      <Route path="/teams" element={<TeamPage />} />
+      <Route path="/leaderboard/:hackathonId" element={<Leaderboard />} />
 
-      <Route path="/register/:hackathonId" element={<Register />} />
-
-      <Route path="/my-registrations" element={<MyRegistrations />} />
+      {/* ================= PARTICIPANT ================= */}
 
       <Route
         path="/participant/dashboard"
@@ -57,6 +69,62 @@ function AppRoutes() {
       />
 
       <Route
+        path="/teams"
+        element={
+          <ProtectedRoute role="participant">
+            <TeamPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/register/:hackathonId"
+        element={
+          <ProtectedRoute role="participant">
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/my-registrations"
+        element={
+          <ProtectedRoute role="participant">
+            <MyRegistrations />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/my-submissions"
+        element={
+          <ProtectedRoute role="participant">
+            <MySubmissions />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/submission/create"
+        element={
+          <ProtectedRoute role="participant">
+            <CreateSubmission />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/submission/edit/:id"
+        element={
+          <ProtectedRoute role="participant">
+            <EditSubmission />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ================= ORGANIZER ================= */}
+
+      <Route
         path="/organizer/dashboard"
         element={
           <ProtectedRoute role="organizer">
@@ -65,19 +133,43 @@ function AppRoutes() {
         }
       />
 
-      <Route path="/organizer/create-hackathon" element={<CreateHackathon />} />
+      <Route
+        path="/organizer/create-hackathon"
+        element={
+          <ProtectedRoute role="organizer">
+            <CreateHackathon />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/organizer/my-hackathons" element={<MyHackathons />} />
+      <Route
+        path="/organizer/my-hackathons"
+        element={
+          <ProtectedRoute role="organizer">
+            <MyHackathons />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/organizer/edit-hackathon/:id" element={<EditHackathon />} />
+      <Route
+        path="/organizer/edit-hackathon/:id"
+        element={
+          <ProtectedRoute role="organizer">
+            <EditHackathon />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/organizer/registrations/:id" element={<Registrations />} />
+      <Route
+        path="/organizer/registrations/:id"
+        element={
+          <ProtectedRoute role="organizer">
+            <Registrations />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/my-submissions" element={<MySubmissions />} />
-
-      <Route path="/submission/create" element={<CreateSubmission />} />
-
-      <Route path="/submission/edit/:id" element={<EditSubmission />} />
+      {/* ================= JUDGE ================= */}
 
       <Route
         path="/judge/dashboard"
@@ -88,11 +180,25 @@ function AppRoutes() {
         }
       />
 
-      <Route path="/judge/submissions" element={<JudgeSubmissions />} />
+      <Route
+        path="/judge/submissions"
+        element={
+          <ProtectedRoute role="judge">
+            <JudgeSubmissions />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/judge/review/:id" element={<ReviewSubmission />} />
+      <Route
+        path="/judge/review/:id"
+        element={
+          <ProtectedRoute role="judge">
+            <ReviewSubmission />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/leaderboard/:hackathonId" element={<Leaderboard />} />
+      {/* ================= ADMIN ================= */}
 
       <Route
         path="/admin/dashboard"
@@ -112,7 +218,16 @@ function AppRoutes() {
         }
       />
 
-      <Route path="/admin/create-user" element={<CreateUser />} />
+      <Route
+        path="/admin/create-user"
+        element={
+          <ProtectedRoute role="admin">
+            <CreateUser />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ================= COMMON AUTH ROUTES ================= */}
 
       <Route
         path="/profile"
@@ -123,7 +238,10 @@ function AppRoutes() {
         }
       />
 
+      {/* ================= 404 ================= */}
+
       <Route path="*" element={<NotFound />} />
+
     </Routes>
   );
 }
