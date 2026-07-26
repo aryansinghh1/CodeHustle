097413a8c590bbef1592c1bcdb0c9fd53169f5e6
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-26 13:33:57 +0530

    protected routes

--- Frontend/src/routes/ProtectedRoutes.jsx
+++ Frontend/src/routes/ProtectedRoutes.jsx
@@ -1,15 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-function ProtectedRoute({ children, roles }) {
+function ProtectedRoute({
+  children,
+  role,
+}) {
   const { user } = useAuth();
 
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" />;
   }
 
-  if (roles && !roles.includes(user.role)) {
-    return <Navigate to="/" replace />;
+  if (role && user.role !== role) {
+    return <Navigate to="/" />;
   }
 
   return children;
