8b7a7c6b33d35b3bdbdf043404f67543e0209006
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-07-26 15:36:32 +0530

    adding loader in page

--- Frontend/src/components/common/Loader.jsx
+++ Frontend/src/components/common/Loader.jsx
@@ -1,5 +1,6 @@
 function Loader({ text = "Loading..." }) {
   return (
+    
     <div className="loader-box">
       <div className="loader-spinner" />
       <p className="text-muted text-sm font-semibold">{text}</p>
